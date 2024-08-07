4b1da5eea863df4c7e0219760a2ba9ebfa7d627f
Benjamin Vigie <vigie.benjamin@outlook.com>
2024-08-07 04:17:13 +0000

    Updaet the config to allow one configuration

--- custom_components/bixi/config_flow.py
+++ custom_components/bixi/config_flow.py
@@ -26,6 +26,8 @@ class BixiConfigFlow(ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle the initial step."""
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
         errors: dict[str, str] = {}
         if user_input is not None:
             if (
